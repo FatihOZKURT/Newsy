012b12c9e070910d8db5b3dbf74628bb38db6f3d
fatih <fatih_ozkurt@hotmail.com>
2026-04-17 16:20:24 +0300

    First commit with <3

---
+++ app/src/main/java/com/example/newsy/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.newsy
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.example.newsy.ui.theme.NewsyTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NewsyTheme {
+
+            }
+        }
+    }
+}
+
